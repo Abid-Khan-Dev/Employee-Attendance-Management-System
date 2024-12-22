6322f6662c10a1cab99e3c4d400c88e1f9943270
Abid Khan <abidkhan2381@gmail.com>
2024-12-22 22:27:42 +0500

    Fixed issues in dashboard and removed unused packages from package.json

--- config/db.js
+++ config/db.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 module.exports = connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('Database connected');
+        console.log('✅ Database connected successfully');
     }
     catch(err){
-        console.log(err.message);
+        console.error('❌ MongoDB connection error:', err.message);
+        console.error('Full Error Details:', err);
         process.exit(1);
     }
 }
--- routes/admin/dashboard.js
+++ routes/admin/dashboard.js
@@ -3,15 +3,18 @@ const router = express.Router();
 const empModel = require('../../models/User');
 const depModel = require('../../models/Department');
 const shiftModel = require('../../models/Shift');
+const leaveModel = require('../../models/Leave');
 
 router.get('/', async(req, res) => {
    const employees = await empModel.find({role: "employee"});
    const departments = await depModel.find();
    const shifts = await shiftModel.find();
+   const leaves = await leaveModel.find().populate('employeeId');
    res.render('./admin/dashboard',{
       employees,
       departments,
-      shifts
+      shifts,
+      leaves
    })
 });
 
