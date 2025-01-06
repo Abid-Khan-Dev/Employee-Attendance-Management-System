8b6e3bd9d6885404ab2480e1b35133ee77617d46
Abid Khan <abidkhan2381@gmail.com>
2025-01-06 15:56:30 +0500

    UI/UX enhancements, DataTables integration, and bug fixes.

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,48 +1,45 @@
 let middlewareObject = {};
 
-// This middleware adds the logged-in user (employee) data to res.locals, so it can be accessed globally in views (like the topbar). 
+// This middleware adds the logged-in user (employee) data to res.locals, so it can be accessed globally in views (like for the topbar).
 middlewareObject.addEmployeeToLocals = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        res.locals.employee = req.user;  
-    } else {
-        res.locals.employee = null;  
-    }
-    next();
-}
-
+  if (req.isAuthenticated()) {
+    res.locals.employee = req.user;
+  } else {
+    res.locals.employee = null;
+  }
+  next();
+};
 
 // isNotLoggedIn
 middlewareObject.isNotLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-         return next();
-    }
-    res.redirect('/dashboard');
+  if (!req.isAuthenticated() || (!req.user && req.user.role !== "employee")) {
+    return next();
+  }
+  next();
 };
 
 // isLoggedIn
 middlewareObject.isLoggedIn = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/auth/login');
+  if (req.isAuthenticated() && req.user && req.user.role === "employee") {
+    return next();
+  }
+  res.redirect("/auth/login");
 };
 
-
-
-// for admin 
-middlewareObject.isNotAdmin = (req, res, next)=>{
-    if(!req.isAuthenticated() || !req.user && req.user.role !== 'admin'){
-        return next();
-    }
-    res.redirect('/admin/dashboard');
-}
+// for admin
+middlewareObject.isNotAdmin = (req, res, next) => {
+  if (!req.isAuthenticated() || (!req.user && req.user.role !== "admin")) {
+    return next();
+  }
+  res.redirect("/admin/dashboard");
+};
 
 // for admin Protected
-middlewareObject.isAdmin = (req, res, next)=>{
-    if(req.isAuthenticated() && req.user && req.user.role === 'admin'){
-        return next();
-    }
-    res.redirect('/admin/auth/login');
-}
+middlewareObject.isAdmin = (req, res, next) => {
+  if (req.isAuthenticated() && req.user && req.user.role === "admin") {
+    return next();
+  }
+  res.redirect("/admin/auth/login");
+};
 
 module.exports = middlewareObject;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,73 +1,75 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-
-// for sidebar toggle
-document.getElementById('toggleSidebar').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('active');
-  });
-
- 
- 
-
-    // it will select all the inputs with the 'form-control datepicker' class
-    const dateInputs = document.querySelectorAll('[data-datepicker="true"]');
-
-    // Initialize Datepicker for each input
-    dateInputs.forEach((input) => {
-      new Datepicker(input, {
-        autohide: true,
-        format: 'yyyy-mm-dd',
-      });
+  // for sidebar toggle
+  document
+    .getElementById("toggleSidebar")
+    .addEventListener("click", function () {
+      const sidebar = document.getElementById("sidebar");
+      sidebar.classList.toggle("active");
     });
 
-    let closeAlertEl = document.getElementById('closeAlert')
-    if(closeAlertEl){
-      setTimeout(() => {
-        let alert = new bootstrap.Alert(closeAlertEl);
-        alert.close();
-      }, 5000);
-    }
+  // it will select all the inputs with the 'form-control datepicker' class
+  const dateInputs = document.querySelectorAll('[data-datepicker="true"]');
 
+  // Initialize Datepicker for each input
+  dateInputs.forEach((input) => {
+    new Datepicker(input, {
+      autohide: true,
+      format: "yyyy-mm-dd",
+    });
+  });
 
-
- function updateTime(){
-  const d = new Date();
-  const currentTimeEl = document.getElementById('currentTime');
-  
-  if(currentTimeEl){
-
-    currentTimeEl.textContent = d.toLocaleTimeString();
+  let closeAlertEl = document.getElementById("closeAlert");
+  if (closeAlertEl) {
+    setTimeout(() => {
+      let alert = new bootstrap.Alert(closeAlertEl);
+      alert.close();
+    }, 5000);
   }
 
- }
- updateTime();
- setInterval(updateTime,1000)
- 
+  function updateTime() {
+    const d = new Date();
+    const currentTimeEl = document.getElementById("currentTime");
 
-    const forms = document.querySelectorAll('#checkInForm,#checkOutForm');
+    if (currentTimeEl) {
+      currentTimeEl.textContent = d.toLocaleTimeString();
+    }
+  }
+  updateTime();
+  setInterval(updateTime, 1000);
 
-    forms.forEach(form => {
-      form.addEventListener('submit',(e)=>{
-        // console.log(form.id);
-        
-        const message = form.id === 'checkInForm' ? 'Are you sure you want to check-in?' : 'Are you sure you want to check-out?'
+  const forms = document.querySelectorAll("#checkInForm,#checkOutForm");
 
-        const userConformed = confirm(message);
-  
-        // It will only prevent the form when the user click on No button so the value will null means false, if clicked on the Yes then this will skip and form will submit
-        if(!userConformed){
-          e.preventDefault();
-        }
-      })
+  forms.forEach((form) => {
+    form.addEventListener("submit", (e) => {
+      // console.log(form.id);
+
+      const message =
+        form.id === "checkInForm"
+          ? "Are you sure you want to check-in?"
+          : "Are you sure you want to check-out?";
+
+      const userConformed = confirm(message);
+
+      // It will only prevent the form when the user click on No button so the value will null means false, if clicked on the Yes then this will skip and form will submit
+      if (!userConformed) {
+        e.preventDefault();
+      }
     });
-    
-
-
-
-
-
   });
 
-
-
+  new DataTable("#dataTable", {
+    paging: true,
+    searching: true,
+    info: true,
+    autoWidth: false,
+    language: {
+      emptyTable: "No data available in the table",
+    },
+    columnDefs: [
+      {
+        targets: "_all", // Apply this to all columns
+        defaultContent: "", // Prevent errors for missing data
+      },
+    ],
+  });
+});
